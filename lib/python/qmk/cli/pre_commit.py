c60e536f891f41a26822f5ef41bd170ed4cbad30
Nick Brassel <nick@tzarc.org>
2025-06-07 22:49:22 +1000

    Passthru.

--- lib/python/qmk/cli/pre_commit.py
+++ lib/python/qmk/cli/pre_commit.py
@@ -2,10 +2,12 @@
 """
 from milc import cli
 
+@cli.argument('pc_args', nargs='+', arg_only=True, help='Arguments to pass to pre-commit. You\'ll generally want to use a double dash (--) to separate the `pre-commit` arguments from the qmk cli arguments, e.g. `qmk pre-commit -- --help`.')
 @cli.subcommand('Runs pre-commit hooks')
 def pre_commit(cli):
     """Runs pre-commit hooks in the QMK firmware directory.
     """
     # Run pre-commit in the QMK firmware directory
-    result = cli.run(['pre-commit', 'run', '--all-files'], capture_output=False)
+    args = ['pre-commit'] + cli.args.pc_args
+    result = cli.run(args, capture_output=False)
     return result.returncode == 0
