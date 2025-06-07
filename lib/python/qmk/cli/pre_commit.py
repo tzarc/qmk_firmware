28a7f2ec968d59da9886770ec4e49fb561cae53f
Nick Brassel <nick@tzarc.org>
2025-06-07 22:52:05 +1000

    Check for `pre-commit` before executing.

--- lib/python/qmk/cli/pre_commit.py
+++ lib/python/qmk/cli/pre_commit.py
@@ -1,12 +1,19 @@
 """Routes to the `pre-commit` command.
 """
+import shutil
 from milc import cli
+
 
 @cli.argument('pc_args', nargs='+', arg_only=True, help='Arguments to pass to pre-commit. You\'ll generally want to use a double dash (--) to separate the `pre-commit` arguments from the qmk cli arguments, e.g. `qmk pre-commit -- --help`.')
 @cli.subcommand('Runs pre-commit hooks')
 def pre_commit(cli):
     """Runs pre-commit hooks in the QMK firmware directory.
     """
+    res = shutil.which('pre-commit')
+    if res is None:
+        cli.log.error("{fg_red}Can't find `pre-commit` in your path.")
+        return False
+
     # Run pre-commit in the QMK firmware directory
     args = ['pre-commit'] + cli.args.pc_args
     result = cli.run(args, capture_output=False)
